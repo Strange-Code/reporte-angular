dcd660e15589d0e09bb0a20b48760dcb23e557dd
Gustavo Jimenez <gjimenez.code@gmail.com>
2023-03-04 11:06:32 -0500

    wip

---
+++ enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB-QSEtcZIYOxSF-OTD7YQLv71hBGFFXUY',
+    authDomain: 'reporte-nestjs.firebaseapp.com',
+    projectId: 'reporte-nestjs',
+    storageBucket: 'reporte-nestjs.appspot.com',
+    messagingSenderId: '922728401355',
+    appId: '1:922728401355:web:0dfeea16e7673e3dbdce4e',
+    measurementId: 'G-TJDVK84SFH',
+  },
+};
